438b734ef63895b34788124ce047036560e12202
strivelei <strivelei@foxmail.com>
2026-01-29 18:26:42 +0800

    init: 项目初始化

---
+++ src/components/Babylon/components/BabylonArcRotateCamera.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author: strivelei strivelei@foxmail.com
+ * @Date: 2026-01-29 18:10:35
+ * @LastEditors: strivelei strivelei@foxmail.com
+ * @LastEditTime: 2026-01-29 18:16:33
+ * @FilePath: src/components/Babylon/components/BabylonArcRotateCamera.vue
+ * @Description: 轨道类型的相机
+ -->
+
+<script setup lang="ts"></script>
+
+<template></template>
+
+<style scoped></style>
