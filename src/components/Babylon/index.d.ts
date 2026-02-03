af4c9c5ed82e1960f16e4655f7ae97380dd9589b
dalei <striveLei@foxmail.com>
2026-02-03 11:07:15 +0800

    fix 提交缺失的文件
    fix 提交缺失的文件

---
+++ src/components/Babylon/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * @Author: strivelei strivelei@foxmail.com
+ * @Date: 2026-02-02 15:15:25
+ * @LastEditors: strivelei strivelei@foxmail.com
+ * @LastEditTime: 2026-02-02 15:17:40
+ * @FilePath: src/components/Babylon/index.d.ts
+ * @Description: 这是默认设置,可以在设置》工具》File Description中进行配置
+ */
+export interface BabylonProps {
+  // 是否是要webGpu
+  webGpu?: boolean
+}
